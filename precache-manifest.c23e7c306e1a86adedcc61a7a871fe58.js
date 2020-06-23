d6e8bc08f20937d727abea3f4bc79e6360d2e167
Newton Poudel <hinewton25@gmail.com>
2020-06-23 09:53:04 +0545

    Updates

---
+++ precache-manifest.c23e7c306e1a86adedcc61a7a871fe58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d3f5f7b7bbcd7741a06342ea6ed8a4",
+    "url": "/ethbech32addr/index.html"
+  },
+  {
+    "revision": "c6d773696de622c72d57",
+    "url": "/ethbech32addr/static/css/main.547cf1c2.chunk.css"
+  },
+  {
+    "revision": "7303c67bb4cb84faf6ad",
+    "url": "/ethbech32addr/static/js/2.411ab5ec.chunk.js"
+  },
+  {
+    "revision": "e6beab4f232caf3cecc1e168fcbd6ff4",
+    "url": "/ethbech32addr/static/js/2.411ab5ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d773696de622c72d57",
+    "url": "/ethbech32addr/static/js/main.4df8a69a.chunk.js"
+  },
+  {
+    "revision": "8d479f26831bb81acf80",
+    "url": "/ethbech32addr/static/js/runtime-main.1990f67e.js"
+  }
+]);
